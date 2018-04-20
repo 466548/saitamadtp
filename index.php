4e403468d94e9bba95c85c799238bc92a72f1dc7
466548 <t.hara@gmail.com>
2018-04-20 12:48:48 +0900

    cleanup

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
+<?php get_header(); ?>
+<body id="index">
+	<div id="wrapper">
+<?php get_sidebar(); ?>
+		<main>
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	if(have_posts()): while(have_posts()): the_post();
+		remove_filter('the_content', 'wpautop');
+		the_content();
+	endwhile; wp_reset_postdata(); endif;
+?>
+		</main>
+<?php get_footer(); ?>
